1fc35b80de0305ba91715df08eb11e59ba10ed47
Cho-Ching Lu <chochinlu@gmail.com>
2019-07-27 17:54:56 +0800

    upload form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+
+const UploadForm = props => {
+  const [file, setFile] = useState(null);
+  const [err, setErr] = useState(null);
+
+  const handleFileChange = e => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('myImage', file);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    };
+
+    try {
+      const result = await axios.post('/upload', formData, config);
+      console.log({ result });
+    } catch (error) {
+      console.log(error);
+      setErr(error.message);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          name="myImage"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+        <button type="submit">Upload</button>
+      </form>
+      {file && file.name}
+      {err && <p>{err}</p>}
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UploadForm />
       </header>
     </div>
   );
